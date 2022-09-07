4fe87147fbfa247c6437f75c02214a41164b5065
gemmayou <e4988632@gmail.com>
2022-09-07 10:19:41 +0800

    fix: transferAdmin with two steps

--- contracts/developer/AccessControlUpgradeableCustom.sol
+++ contracts/developer/AccessControlUpgradeableCustom.sol
@@ -11,22 +11,106 @@ contract AccessControlUpgradeableCustom is
 {
     using AddressUpgradeable for address;
 
+    /**
+     * Global Variables
+     */
+    mapping(address => address) private _inTransition;
+
+    /**
+     * Events
+     */
+
     event TransferAdmin(address adminAddress);
+    event UpdateAdmin(address oldAdminAddress);
+    event CancelTransferAdmin(address newAdminAddress);
+
+    /**
+     * Errors
+     */
+
+    error ErrIntransition();
+    error ErrNotIntransition();
+    error ErrInvalidTransition();
+    error ErrGrantRoleInValidAddress();
+
+    /**
+     * modifier
+     */
+
+    // check if transition do exist
+    modifier inTransition(address oldAdmin) {
+        if (_inTransition[oldAdmin] == address(0)) {
+            revert ErrNotIntransition();
+        }
+        _;
+    }
+
+    // check there's no transition pending
+    modifier notInTransition() {
+        if (_inTransition[_msgSender()] != address(0)) {
+            revert ErrIntransition();
+        }
+        _;
+    }
+
+    /**
+     * Functions
+     */
 
     function __AccessControlCustom_init() internal onlyInitializing {
         __AccessControl_init();
     }
 
+    /// return pending transition is to whom
+    ///
+    /// @param admin address - the original Admin
+    ///
+    /// @return newAdmin address - whom the Admin role is transfered to
+    function transition(address admin) external view returns (address) {
+        return _inTransition[admin];
+    }
+
+    /// grant admin to newAdmin, and set a pending transition in record
+    ///
+    /// @param newAdmin address - the address that Admin role is transfered to
     function transferAdmin(address newAdmin)
         external
         onlyRole(DEFAULT_ADMIN_ROLE)
+        notInTransition
     {
-        require(
-            newAdmin != address(0),
-            "AccessControlCustom: newAdmin cannot cannot be 0x0"
-        );
+        if (newAdmin == address(0) || newAdmin == _msgSender()) {
+            revert ErrGrantRoleInValidAddress();
+        }
+        _inTransition[_msgSender()] = newAdmin;
         _grantRole(DEFAULT_ADMIN_ROLE, newAdmin);
-        _revokeRole(DEFAULT_ADMIN_ROLE, _msgSender());
         emit TransferAdmin(newAdmin);
+    }
+
+    /// the address who gets admin from transition are able to accept and revoke the old Admin
+    ///
+    /// @param oldAdmin address - the original Admin address
+    function updateAdmin(address oldAdmin)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+        inTransition(oldAdmin)
+    {
+        if (_inTransition[oldAdmin] != _msgSender()) {
+            revert ErrInvalidTransition();
+        }
+        _inTransition[oldAdmin] = address(0);
+        _revokeRole(DEFAULT_ADMIN_ROLE, oldAdmin);
+        emit UpdateAdmin(oldAdmin);
+    }
+
+    /// old Admin can cancel its transition
+    function cancelTransferAdmin()
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+        inTransition(_msgSender())
+    {
+        address adminToBeCanceled = _inTransition[_msgSender()];
+        _inTransition[_msgSender()] = address(0);
+        _revokeRole(DEFAULT_ADMIN_ROLE, adminToBeCanceled);
+        emit CancelTransferAdmin(adminToBeCanceled);
     }
 }
