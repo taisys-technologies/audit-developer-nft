37f1c3d577b4abe4d10a60cadc3130e6a68de98d
GemmaYou <61839846+GemmaYou@users.noreply.github.com>
2023-04-12 10:01:40 +0800

    doc: update devNFT and README (#3)

--- contracts/developer/ERC721AStorageCustom.sol
+++ contracts/developer/ERC721AStorageCustom.sol
@@ -3,23 +3,48 @@
 pragma solidity 0.8.4;
 
 library ERC721AStorageCustom {
+    struct Level {
+        uint256 price;
+        // maximum agent amount
+        uint256 maxAgentAmount;
+    }
+
+    struct Token {
+        uint256 level;
+        uint256 levelVersion;
+    }
+
+    struct TokenWithLevel {
+        uint256 tokenId;
+        uint256 levelNum;
+        Level level;
+    }
+
     struct Layout {
         // all periods sum up
-        uint256 _availableTokenSupply;
-        // current period
+        uint256 _sumOfPeriodTokenSupply;
+        // number of sold tokens by level
+        mapping(uint256 => uint256) _soldTokenAmount;
+        // token supply of current period
         uint256 _periodTokenSupply;
-        // contract max NFT
-        uint256 _maxTokenSupply;
         // signer who authorize mint (backend)
         address _signerAddress;
-        // record used uuid
-        mapping(string => bool) _usedUUID;
         // paytment token
         address _paymentContract;
-        // price of per NFT
-        uint256 _price;
-        // addr who already mint
-        mapping(address => bool) _minted;
+        // record used uuid
+        mapping(string => bool) _usedUUID;
+        // record current level version
+        uint256 _levelVersion;
+        // record level info by version
+        mapping(uint256 => Level[]) _levels;
+        // record each level is mintable or not
+        mapping(uint256 => bool) _mintable;
+        // record each level's maximum supply
+        mapping(uint256 => uint256) _levelTokenSupply;
+        // sum up of all level's supply
+        uint256 _sumOfLevelTokenSupply;
+        // each tokenId matching with level
+        mapping(uint256 => Token) _tokenLevel;
     }
 
     bytes32 internal constant STORAGE_SLOT =
